7e920bb69693586656a831a51ef9606ee0a6ae02
Plastic_X_Plastic <80278929+PlasticxPlastic@users.noreply.github.com>
2021-08-30 14:04:07 +0700

    Update GAME_LAB.cpp

--- Game_lab.cpp/GAME_LAB.cpp
+++ Game_lab.cpp/GAME_LAB.cpp
@@ -1,65 +1,54 @@
-#include<iostream>
-#include<windows.h>
-#include<time.h>
-#include<conio.h>
 #include<stdio.h>
-using namespace std;
+#include<Windows.h>
+#include<conio.h>
+#include<iostream>
+
+void draw_ship(int x, int y)
+{
+	gotoxy(x,y);
+	printf(" <-0-> ");
+}
 
 void gotoxy(int x, int y)
 {
-	COORD c = { x, y };
+	COORD c = { x,y };
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
 }
-void draw_ship(int x, int y)
-{
-	gotoxy(x, y); 
-	printf(" <-0-> ");
-}
-void draw_bullet(int x, int y)
-{
-	gotoxy(x, y); printf("^");
-}
+
 void erase_ship(int x, int y)
 {
-	gotoxy(x, y); printf("       ");
+	gotoxy(x, y);
+	printf("       ");
 }
+
 int main()
 {
 	char ch = '.';
 	int x = 38, y = 20;
 	draw_ship(x, y);
 	do {
-		if (_kbhit()) {
+		if (_kbhit())
+		{
 			ch = _getch();
-			if (ch == 'a') {
-				if (x != 1) { draw_ship(--x, y); }
+			if (ch == 'a')
+			{
+				if (x != 1) { draw_ship(--x, y);}
 				else { draw_ship(x, y); }
 			}
-			if (ch == 'd') {
+			if (ch == 'd')
+			{
 				if (x != 80) { draw_ship(++x, y); }
 				else { draw_ship(x, y); }
 			}
-			if (ch == 'w') {
-				if (y != 1)
-				{
-					erase_ship(x, y);
-					draw_ship(x, --y);
-				}
-				else
-				{
-					draw_ship(x, y);
-				}
+			if (ch == 'w')
+			{
+				if (y != 1) { erase_ship(x, y); draw_ship(x, --y); }
+				else { draw_ship(x, y); }
 			}
-			if (ch == 's') {
-				if (y != 23)
-				{
-					erase_ship(x, y);
-					draw_ship(x, ++y);
-				}
-				else
-				{
-					draw_ship(x, y);
-				}
+			if (ch == 's')
+			{
+				if (y != 24) { erase_ship(x, y); draw_ship(x, ++y); }
+				else { draw_ship(x, y); }
 			}
 			fflush(stdin);
 		}
